4ea9c19e1f2e2875e445d5a145d650169b5e13f4
Marcus Hallman <hallman@wustl.edu>
2018-02-22 11:52:14 -0600

    Finished up my playgame function

--- ProblemSet4.R
+++ ProblemSet4.R
@@ -1,7 +1,7 @@
 # First Steps Original
 
 myFunction<-function(doorthing, doorthing2, x){  #I do not understand why you have a third arguement. maybe I'm missing something
-  doorthing1<-doorthing2<-sample(1:3, 1) #This should name the size with 'size=1'. Also, doorthing1 should be called doorthing. Additionally, we should be taking in player inputs ratehr than generating them.
+  doorthing1<-doorthing2<-sample(1:3, 1) #doorthing1 should be called doorthing. Additionally, we should be taking in player inputs ratehr than generating them.
   if (doorthing1==doorthing2){ x<-TRUE } else { x==FALSE } #This is a mess to look at, also there should not be an equality logical in the else clause
   x
 }
@@ -36,7 +36,7 @@ setClass(Class="door",
            switch=c()
          )
 )
-
+# A validity checker
 setValidity("door", function(object){
   carWorks<-(object@carDoor==1 | object@carDoor==2 | object@carDoor==3)
   chosenWorks<-(object@chosenDoor==1|object@chosenDoor==2|object@chosenDoor==3)
@@ -44,7 +44,7 @@ setValidity("door", function(object){
 })
 
 
-
+# A constructor
 setMethod("initialize", "door", function(.Object, ...){
   value = callNextMethod()
   validObject(value)
@@ -54,17 +54,37 @@ setMethod("initialize", "door", function(.Object, ...){
 
 new("door",chosenDoor=4,carDoor=2,switch=FALSE)
 
-setGeneric("PlayGame",
-           function(object="door") {
-             standardGeneric("door")
-           })
+
+
+# a generic PlayGame
+setGeneric("PlayGame", function(object="door"){   
+  standardGeneric("PlayGame")
+})
 
 setMethod("PlayGame", "door",
           function(object){
-            return(object)
-          } )
-
-
+            object@carDoor<-sample(1:3,size=1) #Pick a random door for the car to be behind
+            pickADoor<-sample(1:3,size=1) #Picks a random door
+            if (object@switch==FALSE){object@chosenDoor<-pickADoor} #If switch is FALSE, player door is randomly that door
+            if (object@switch==TRUE){       # if player switches, we open a door.
+              hold<-FALSE
+              openDoor<-0
+              while(hold==FALSE){ 
+                openDoor<-sample(1:3,size=1)     # randomly choose a door to remove from consideration that is not the car or first chosen door
+                if (openDoor!=object@carDoor & openDoor!=pickADoor){hold=TRUE}
+              }
+              validSwitch<-FALSE
+              switchedDoor<-0
+              while(validSwitch==FALSE){   #making sure we pick the door that is not the first door we picked and not the open door
+                switchedDoor<-sample(1:3,size=1)
+                if(switchedDoor!=pickADoor & switchedDoor!=openDoor){
+                  validSwitch=TRUE}
+              }
+              object@chosenDoor<-switchedDoor
+            }
+            if (object@chosenDoor==object@carDoor){return("WINNER!")}  #Return the proper result. Winner if chosen door and car door are the same, goat otherwise
+            else{return("GOAT!")}
+          })
 
 
 
