53f522b9d4789c76a722a273723fa96e7a37b229
Marcus Hallman <hallman@wustl.edu>
2018-02-22 11:16:02 -0600

    Changes to getting started

--- ProblemSet4.R
+++ ProblemSet4.R
@@ -1,8 +1,8 @@
 # First Steps Original
 
-myFunction<-function(doorthing, doorthing2, x){  #I do not understand why you have a third arguement
-  doorthing1<-doorthing2<-sample(1:3, 1) #This should name the size with 'size=1'. Also, Doorthing1 should be called 
-  if (doorthing1==doorthing2){ x<-TRUE } else { x==FALSE } #This is a mess to look at, also there should not be an equality logical
+myFunction<-function(doorthing, doorthing2, x){  #I do not understand why you have a third arguement. maybe I'm missing something
+  doorthing1<-doorthing2<-sample(1:3, 1) #This should name the size with 'size=1'. Also, doorthing1 should be called doorthing. Additionally, we should be taking in player inputs ratehr than generating them.
+  if (doorthing1==doorthing2){ x<-TRUE } else { x==FALSE } #This is a mess to look at, also there should not be an equality logical in the else clause
   x
 }
 myFunction(sample(1:3, 1), sample(1:3, 1))
@@ -11,17 +11,13 @@ myFunction(sample(1:3, 1), sample(1:3, 1))
 
 
 #First Steps edited
-myFunction<-function(doorthing1, doorthing2){
-  x<-NULL
-  doorthing1<-sample(1:3, size=1)
-  doorthing2<-sample(1:3, size=1)
+myFunction<-function(doorthing1, doorthing2){  
+  x<-NULL  
   if (doorthing1==doorthing2){ x=TRUE } 
   else { x=FALSE }
   return(x)
 }
 myFunction(sample(1:3, size=1), sample(1:3, size=1))
-# Should return a TRUE if these samples are equal and
-# a false if they are not
 
 
 #Problem Set #4
@@ -44,7 +40,31 @@ setClass(Class="door",
 setValidity("door", function(object){
   carWorks<-(object@carDoor==1 | object@carDoor==2 | object@carDoor==3)
   chosenWorks<-(object@chosenDoor==1|object@chosenDoor==2|object@chosenDoor==3)
-  if(!carWorks & !chosenWorks){return("Pick numbers 1,2,3 for carDoor or chosenDoor")}  #This checks that doors are 1,2,or3)
+  if(!carWorks | !chosenWorks){return("Pick numbers 1,2,3 for carDoor or chosenDoor")}  #This checks that doors are 1,2,or3)
 })
 
-new("door",chosenDoor=4,carDoor)
+
+
+setMethod("initialize", "door", function(.Object, ...){
+  value = callNextMethod()
+  validObject(value)
+  return(value)
+})
+
+
+new("door",chosenDoor=4,carDoor=2,switch=FALSE)
+
+setGeneric("PlayGame",
+           function(object="door") {
+             standardGeneric("door")
+           })
+
+setMethod("PlayGame", "door",
+          function(object){
+            return(object)
+          } )
+
+
+
+
+
