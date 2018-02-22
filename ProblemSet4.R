48a65f6419c73c203d8342afad0c653a3968f21d
Marcus Hallman <hallman@wustl.edu>
2018-02-22 10:55:53 -0600

    Set Classes and Validators

--- ProblemSet4.R
+++ ProblemSet4.R
@@ -26,3 +26,25 @@ myFunction(sample(1:3, size=1), sample(1:3, size=1))
 
 #Problem Set #4
 
+#Problem #1
+
+setClass(Class="door",
+         representation = representation(
+           chosenDoor="numeric",
+           carDoor="numeric",
+            switch="logical"
+         ),
+         prototype = prototype(
+           chosenDoor = c(),
+           carDoor=c(),
+           switch=c()
+         )
+)
+
+setValidity("door", function(object){
+  carWorks<-(object@carDoor==1 | object@carDoor==2 | object@carDoor==3)
+  chosenWorks<-(object@chosenDoor==1|object@chosenDoor==2|object@chosenDoor==3)
+  if(!carWorks & !chosenWorks){return("Pick numbers 1,2,3 for carDoor or chosenDoor")}  #This checks that doors are 1,2,or3)
+})
+
+new("door",chosenDoor=4,carDoor)
